ce9451569ab58dceef7ac704b5b5d62d462c04f0
Justyna Rozycka <justyna.rozycka@o2.pl>
2022-05-16 12:37:29 +0200

    main image size

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,21 +1,7 @@
 <template>
   <v-img
     :src="getImageUrl()"
-    height="600"
-    class="bg-grey-lighten-2 mt-16"
-    cover
-  ></v-img>
-
-  <v-img
-    :src="getImageUrl()"
-    height="600"
-    class="bg-grey-lighten-2 mt-16"
-    cover
-  ></v-img>
-
-  <v-img
-    :src="getImageUrl()"
-    height="600"
+    :height="height"
     class="bg-grey-lighten-2 mt-16"
     cover
   ></v-img>
@@ -28,7 +14,24 @@ export default {
   data: () => ({}),
   methods: {
     getImageUrl() {
-      return new URL(`../assets/maszyna.jpg`, import.meta.url).href;
+      return new URL(`../assets/mitsuPoziomo.jpg`, import.meta.url).href;
+    },
+  },
+  computed: {
+    height() {
+      console.log(this.$vuetify.display.name);
+      switch (this.$vuetify.display.name) {
+        case "xs":
+          return 220;
+        case "sm":
+          return 400;
+        case "md":
+          return 400;
+        case "lg":
+          return 500;
+        case "xl":
+          return 600;
+      }
     },
   },
 };
