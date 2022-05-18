3bb886b62e2aa5b2fc0f502db256f449c0458c42
Justyna Rozycka <justyna.rozycka@o2.pl>
2022-05-18 20:20:08 +0200

    web hash history

--- src/main.js
+++ src/main.js
@@ -4,16 +4,14 @@ import router from './router'
 import vuetify from './plugins/vuetify'
 import { loadFonts } from './plugins/webfontloader'
 import defaultLayout from "./defaultLayout.vue";
-/* import the fontawesome core */
 import { library } from '@fortawesome/fontawesome-svg-core'
-
-/* import specific icons */
 import { faUserSecret } from '@fortawesome/free-solid-svg-icons'
+import VueCookieComply from 'vue-cookie-comply'
+import 'vue-cookie-comply/dist/style.css'
 
-/* add icons to the library */
+
 library.add(faUserSecret)
 
-/* add font awesome icon component */
 
 
 loadFonts()
@@ -21,6 +19,7 @@ loadFonts()
 const app = createApp(App)
   .use(router)
   .use(vuetify)
+  .use(VueCookieComply)
 app.component("defaultLayout", defaultLayout);
 app.directive('scroll', {
   inserted: function (el, binding) {
